616853ac5e26fc21b145abfa6e97ef58c54389c5
ryeongse25 <seryeong971120@gmail.com>
2024-07-09 11:25:12 +0900

    feat: login.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Main from "./pages/Main";
 import Report from "./pages/Report";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
         <Route path='/' element={<Home />}></Route>
         <Route path='/main' element={<Main />}></Route>
         <Route path='/report' element={<Report />}></Route>
-        <Route path='/signin'></Route>
+        <Route path='/login' element={<Login />}></Route>
         <Route path='/signup'></Route>
       </Routes>
       </BrowserRouter>
---
+++ src/pages/Login.jsx
@@ -0,0 +1,7 @@
+const Login = () => {
+  return <>
+    민선 코드
+  </>
+}
+
+export default Login;
