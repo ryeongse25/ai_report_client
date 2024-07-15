0e94e0042ab2b40a8103765b9f65fab159464f7c
ryeongse25 <seryeong971120@gmail.com>
2024-07-15 17:28:58 +0900

    feat: signup

--- src/pages/signup/SignUp.jsx
+++ src/pages/signup/SignUp.jsx
@@ -27,74 +27,46 @@ function Signup() {
   const [id, setId] = useState('');
   const [idDuplicated, setIdDuplicated] = useState(false);
 
-  const [password, setPassword] = useState('');
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
-  const [authNumVisible, setAuthNumVisible] = useState(true);
+  const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
-  const [authNum, setAuthNum] = useState('');
 
+  const [code, setCode] = useState('');
+  const [confirmed, setConfirmed] = useState(false); // 이메일 인증 완료 여부
+  const [authNumVisible, setAuthNumVisible] = useState(false);
+
+  // 스페이스바 막기
   const onKeyDown = (e) => {
     if (e.key === ' ') e.preventDefault();
   }
 
-  // email 유효성 검사
-  const checkEmailAvailability = async (email) => {
-    try {
-      const response = await axios.post('/', { email }); // api 주소
-      if (response.data.isAvailable) {
-        Swal.fire({
-          icon: 'success',
-          title: '인증번호가 발송되었습니다',
-          showConfirmButton: false,
-          timer: 2000
-        });
-        setAuthNumVisible(true);
-      } else {
-        Swal.fire({
-          icon: 'error',
-          title: '이미 가입된 이메일입니다',
-          showConfirmButton: false,
-          timer: 2000
-        });
-      }
-    } catch (error) {
-      console.error('Error checking email availability:', error);
-      Swal.fire({
-        icon: 'error',
-        title: '이메일 중복 검사에 실패했습니다',
-        showConfirmButton: false,
-        timer: 2000
-      });
-    }
-  };
-
   // email 인증번호 확인
-  const handleAuthNumCheck = async () => {
-    try {
-      const response = await axios.post('/', { email, authNum }); // api 주소
-      if (response.data.isValid) {
-        Swal.fire({
-          icon: 'success',
-          title: '인증번호가 확인되었습니다',
-          showConfirmButton: true
-        });
-      } else {
-        Swal.fire({
-          icon: 'error',
-          title: '인증번호가 올바르지 않습니다',
-          showConfirmButton: true
-        });
-      }
-    } catch (error) {
-      console.error('Error checking auth number:', error);
-      Swal.fire({
-        icon: 'error',
-        title: '인증번호 확인에 실패했습니다',
-        showConfirmButton: true
-      });
-    }
-  };
+  // const handleAuthNumCheck = async () => {
+  //   try {
+  //     const response = await axios.post('/', { email, code }); // api 주소
+  //     if (response.data.isValid) {
+  //       Swal.fire({
+  //         icon: 'success',
+  //         title: '인증번호가 확인되었습니다',
+  //         showConfirmButton: true
+  //       });
+  //     } else {
+  //       Swal.fire({
+  //         icon: 'error',
+  //         title: '인증번호가 올바르지 않습니다',
+  //         showConfirmButton: true
+  //       });
+  //     }
+  //   } catch (error) {
+  //     console.error('Error checking auth number:', error);
+  //     Swal.fire({
+  //       icon: 'error',
+  //       title: '인증번호 확인에 실패했습니다',
+  //       showConfirmButton: true
+  //     });
+  //   }
+  // };
    
   // 아이디 중복 검사
   const onIdChange = (e) => {
@@ -115,37 +87,91 @@ function Signup() {
     })
   };
 
-  const onNameChange = (e) => {
-    setName(e.target.value);
+  // 이메일 인증 버튼 클릭
+  const onEmailClick = () => {
+    const csrftoken = getCookie('csrftoken');
+    axios.post('http://localhost:8000/account/emailcheck/', {'email': email}, {
+      headers: {
+        'X-CSRFToken': csrftoken
+      }
+    })
+    .then((res) => {
+      // 이메일 중복 검사 성공
+      if (!res.data.valid) {
+        Swal.fire({
+          icon: 'error',
+          title: '이미 가입된 이메일입니다.',
+          showConfirmButton: false,
+          timer: 2000
+        });
+      } else {
+        // 이메일 인증코드 보내는 api 호출
+        axios.post('http://localhost:8000/account/sendemail/', {'email': email}, {
+          headers: {
+            'X-CSRFToken': csrftoken
+          }
+        })
+        .then((res) => {
+          // 이메일 발송 성공
+          console.log(res);
+          Swal.fire({
+            icon: 'success',
+            title: '인증번호가 발송되었습니다',
+            showConfirmButton: false,
+            timer: 2000
+          });
+          setAuthNumVisible(true);
+        })
+        .catch((error) => {
+          // 이메일 발송 실패
+          console.log(error);
+          Swal.fire({
+            icon: 'error',
+            title: '이메일 발송에 실패하였습니다.',
+            text: '잠시후 다시 시도해주세요.',
+            showConfirmButton: false,
+            timer: 2000
+          });
+        })
+      }
+    })
+    .catch((error) => {
+      // 이메일 중복 확인 실패
+      console.log(error)
+    })
   }
 
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const handleAuthButtonClick = () => {
-    checkEmailAvailability(email);
-  };
-
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const handleConfirmPasswordChange = (e) => {
-    setConfirmPassword(e.target.value);
-  };
-
-  const handleAuthNumChange = (e) => {
-    setAuthNum(e.target.value);
-  };
+  // 인증 코드 확인
+  const onClickAuthBtn = () => {
+    const csrftoken = getCookie('csrftoken');
+    axios.post('http://localhost:8000/account/checksignupcode/', {email, code}, {
+      headers: {
+        'X-CSRFToken': csrftoken
+      }
+    })
+    .then((res) => {
+      console.log(res.data)
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
   
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (!name || !id || !password || !confirmPassword || !email || !authNum) {
+  // 가입하기 버튼
+  const handleSubmit = async () => {
+    if (!name || !id || !password || !confirmPassword || !email) {
       Swal.fire({
         icon: 'warning',
         title: '모든 정보를 입력해주세요',
-        showConfirmButton: true,
+        showConfirmButton: false,
+        timer: 2000
+      });
+      return;
+    } else if (!confirmed) {
+      Swal.fire({
+        icon: 'warning',
+        title: '이메일 인증을 완료해주세요.',
+        showConfirmButton: false,
         timer: 2000
       });
       return;
@@ -163,7 +189,7 @@ function Signup() {
     }
 
     try {
-      const response = await axios.post('/', { name, id, password, email, authNum }); // api 주소
+      const response = await axios.post('/', { name, id, password, email, code }); // api 주소
       if (response.data.success) {
         Swal.fire({
           icon: 'success',
@@ -207,7 +233,7 @@ function Signup() {
                 <div className="inlineFieldContainer">
                   <div className="fieldContainer">
                     <p><label htmlFor="name">name</label></p>
-                    <input placeholder="이름" value={name} onChange={onNameChange} onKeyDown={onKeyDown} required />
+                    <input placeholder="이름" value={name} onChange={(e) => setName(e.target.value)} onKeyDown={onKeyDown} required />
                   </div>
                   <div className="fieldContainer">
                     <p><label htmlFor="id">id</label></p>
@@ -226,35 +252,32 @@ function Signup() {
                 <div>
                   <div className="fieldContainer">
                     <label htmlFor="password">pw</label>
-                    <input type="password" id="password" placeholder="비밀번호" value={password} onChange={handlePasswordChange} onKeyDown={onKeyDown} required />
-                    <input type="password" id="confirm-password" placeholder="비밀번호 재확인" value={confirmPassword} onChange={handleConfirmPasswordChange} onKeyDown={onKeyDown} required />
+                    <input type="password" id="password" placeholder="비밀번호" value={password} onChange={(e) => setPassword(e.target.value)} onKeyDown={onKeyDown} required />
+                    <input type="password" id="confirm-password" placeholder="비밀번호 재확인" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} onKeyDown={onKeyDown} required />
                   </div>
                 </div>
                 <div>
                   <div style={{paddingTop: '10px'}}>
                     <label htmlFor="email">e-mail</label>
                     <div class='emailContainer'>
-                      <input 
-                        type="email" 
-                        id="email" 
+                      <input
                         placeholder="이메일" 
-                        onChange={handleEmailChange}
+                        onChange={(e) => setEmail(e.target.value)}
                         onKeyDown={onKeyDown}
                         required 
                       />
-                      <button type="button" className="authButton" onClick={handleAuthButtonClick}>인증</button>
+                      <button type="button" onClick={onEmailClick}>인증</button>
                     </div>
                   </div>
-                  {authNumVisible && (
-                    <div className="authNumContainer">
-                      <input className="authNum" type="text" id="text" placeholder="인증번호" value={authNum} onChange={handleAuthNumChange} required />
-                      <button type="button" className="authNumButton" onClick={handleAuthNumCheck}>확인</button>
-                    </div>
-                  )}
+                  {authNumVisible &&
+                  <div className="authNumContainer">
+                    <input className='authNum' placeholder="인증번호" value={code} onChange={(e) => setCode(e.target.value)} required />
+                    <button type="button" onClick={onClickAuthBtn}>확인</button>
+                  </div>}
                 </div>
               </div>
             </form>
-          <button type="submit" className="submitBtn">가입하기</button>
+          <button type="submit" className="submitBtn" onClick={handleSubmit}>가입하기</button>
         </div>
       </div>
     </FullContainer>
