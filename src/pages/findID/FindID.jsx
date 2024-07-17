43b009892402e5f452543e4ed325dfdf833f7b79
ryeongse25 <seryeong971120@gmail.com>
2024-07-17 10:25:43 +0900

    feat: findid api 연결

--- src/pages/findID/FindID.jsx
+++ src/pages/findID/FindID.jsx
@@ -1,39 +1,43 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { FullContainer } from '../../components/CommonStyles';
-import { GoBackBtn } from '../../components/CommonStyles';
+import { isValidEmail } from '../../utils/utils';
+import { errorWithoutBtn, successWithoutBtn } from '../../utils/swal';
+
 import './FindID.css';
+import { GoBackBtn } from '../../components/CommonStyles';
+import { FullContainer } from '../../components/CommonStyles';
+import { findid, verifyid } from '../../apis/user';
 
 function FindID() {
   const navigate = useNavigate();
+  const [id, setId] = useState('');
+  const [code, setCode] = useState('');
   const [email, setEmail] = useState('');
-  const [error, setError] = useState('');
+  const [isCode, setIsCode] = useState(false);
 
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
+  // 인증 메일 발송 버튼
+  const onSend = async () => {
+    if (!email) return;
 
-  const handleEmailSubmit = async () => {
-    try {
-      const response = await fetch('/account/findid', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email }),
-      });
+    // 이메일 유효성 검사
+    const res = isValidEmail(email);
+    if (!res) {
+      errorWithoutBtn('이메일 주소가 올바르지 않습니다.');
+      return;
+    }
 
-      const result = await response.json();
-
-      if (result.valid) {
-        navigate('/find-id-2');
-      } else {
-        setError('해당 이메일이 존재하지 않습니다.');
-      }
-    } catch (error) {
-      setError('서버 오류가 발생했습니다. 나중에 다시 시도해주세요.');
+    const code = await findid(email);
+    if (code) {
+      setIsCode(true);
+      successWithoutBtn('인증번호가 발송되었습니다.', '5분 안에 인증번호를 입력해주세요.', () => {});
     }
   };
+
+  // 인증 코드 확인 버튼
+  const onClick = async () => {
+    const res = await verifyid(email, code);
+    if (res) setId(res);
+  }
 
   return (
     <FullContainer>
@@ -46,21 +50,32 @@ function FindID() {
         <div className='backContainer'>
           <h2 className='title'>아이디 찾기</h2>
           <div className='formContainer'>
-            <div className='description'>이메일 정보를 입력해 주십시오.</div>
-            <div className='form' onSubmit={(e) => e.preventDefault()}>
-              <label htmlFor="email">e-mail</label>
-              <input 
-                id="email" 
-                placeholder="이메일" 
-                value={email}
-                onChange={handleEmailChange}
-                required 
-              />
-              <button type="button" onClick={handleEmailSubmit}>인증메일 발송</button>
-            </div>
-            <div className='error'>
-              {error && <p>{error}</p>}
-            </div>
+            {id == '' ? <>
+              <div className='description'>이메일 정보를 입력해 주십시오.</div>
+              <div className='form'>
+                <label htmlFor="email">e-mail</label>
+                <input
+                  placeholder="이메일" 
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  />
+                <button type="button" onClick={onSend}>인증메일 발송</button>
+              </div>
+              <div className='form' style={{marginTop: '5px'}}>
+                {isCode && <>
+                  <input
+                    placeholder="인증번호" 
+                    value={code}
+                    onChange={(e) => setCode(e.target.value)}
+                    />
+                  <button type="button" onClick={onClick}>확인</button>
+                </>}
+              </div>
+            </> : 
+            <>
+              <p className='id-result'>아이디 : {id}</p>
+              <button className='login' onClick={() => navigate('/login')}>로그인 페이지로 이동</button>
+            </>}
           </div>
       </div>
     </div>
