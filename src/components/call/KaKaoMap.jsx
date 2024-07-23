198d2fce252e3a9d627a172f7d72f515c116260d
ryeongse25 <seryeong971120@gmail.com>
2024-07-23 14:42:17 +0900

    fix: error

--- src/components/call/KaKaoMap.jsx
+++ src/components/call/KaKaoMap.jsx
@@ -2,34 +2,34 @@ import React, { useEffect } from 'react';
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
 
 const KakaoMap = ({lat, lng}) => {
-    useEffect(() => {
-        const script = document.createElement('script');
-        script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAO_API_KEY}&autoload=false`;
-        script.async = true;
-        document.head.appendChild(script);
-    
-        script.onload = () => {
-          window.kakao.maps.load(() => {
-            const container = document.getElementById('map');
-            const options = {
-              center: new window.kakao.maps.LatLng(lat, lng),
-              level: 3
-            };
-            const map = new window.kakao.maps.Map(container, options);
-    
-            const markerPosition  = new window.kakao.maps.LatLng(lat, lng);
-            const marker = new window.kakao.maps.Marker({
-              position: markerPosition
-            });
-    
-            marker.setMap(map);
-          });
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAO_API_KEY}&autoload=false`;
+    script.async = true;
+    document.head.appendChild(script);
+
+    script.onload = () => {
+      window.kakao.maps.load(() => {
+        const container = document.getElementById('map');
+        const options = {
+          center: new window.kakao.maps.LatLng(lat, lng),
+          level: 3
         };
-      }, []);
+        const map = new window.kakao.maps.Map(container, options);
+
+        const markerPosition  = new window.kakao.maps.LatLng(lat, lng);
+        const marker = new window.kakao.maps.Marker({
+          position: markerPosition
+        });
+
+        marker.setMap(map);
+      });
+    };
+  }, [lat]);
     
-      return (
-        <div id="map" style={{ width: '100%', height: '100%' }}></div>
-      );
+  return (
+    <div id="map" style={{ width: '100%', height: '100%' }}></div>
+  );
 }
 
 export default KakaoMap;
